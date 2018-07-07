98eb4b65a95462732f6d01205fda69103c81268d
Stan.bienaives@gmail.com <stan.bienaives@gmail.com>
2018-07-08 01:30:10 +0200

    lint

--- examples/collect-primes.js
+++ examples/collect-primes.js
@@ -1,28 +1,30 @@
+
+
 const seed = () => {
-  return Array.from(new Array(200), () => Math.floor(Math.random()* 10000))
-}
-const isPrime = num => {
-  for(let i = 2; i < num; i++)
-    if(num % i === 0) return false;
+  return Array.from(new Array(200), () => Math.floor(Math.random() * 10000));
+};
+
+const isPrime = (num) => {
+  for (let int = 2; int < num; int += 1) {
+    if (num % int === 0)
+      return false;
+  }
+
   return num !== 1;
-}
+};
+
 const fitness = (entity) => {
   return entity.reduce((count, gene) => {
     return count + (isPrime(gene) ? 1 : 0);
-  }, 0)
-}
+  }, 0);
+};
+
 const Tournament = require('../lib/genetic');
 
-tournament = new Tournament({
+const tournament = new Tournament({
   seed,
   fitness,
-  config: {
-    size: 200,
-    crossover: 0.1,
-    mutation: 0.1,
-    iterations: 200,
-  }
-})
+});
 
 
 tournament.evolve();
