e3b0c6363cb1eb4f8a59fd6f96e2cf0c833089d0
Stan.bienaives@gmail.com <stan.bienaives@gmail.com>
2018-07-07 10:31:11 +0200

    init

---
+++ tests/genetic.js
@@ -0,0 +1,30 @@
+const test = require('ava');
+const GeneticTournament = require('../lib/genetic');
+
+
+const tournament = new GeneticTournament({
+  seed: () => [...Array(20).keys()].map(() => Math.random()),
+  optimize: (a,b) => a > b,
+  fitness: (entity) => entity.reduce((sum, occ) => sum + occ, 0),
+  config: {
+    size: 20000,
+    crossover: 0.2,
+    mutation: 0.2,
+    iterations: 100,
+    survivals: 1,
+  }
+});
+
+tournament.evolve();
+
+test('Best entity should be very close to an Array of one', function test(t) {
+  const best = tournament.best;
+  const footprint =  best.reduce((sum, occ) => sum + parseFloat(occ), 0); 
+  t.true( (best.length  - footprint) / best.length < 0.01 )
+
+    // notification,
+})
+
+
+
+// })
