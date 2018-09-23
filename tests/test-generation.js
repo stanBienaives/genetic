3fe4eeb7746f0a536881f07b226fcce1980a2fb9
Stan.bienaives@gmail.com <stan.bienaives@gmail.com>
2018-09-23 17:02:13 +0200

    fix selector when population is smaller that 3

--- tests/test-generation.js
+++ tests/test-generation.js
@@ -20,3 +20,15 @@ test('Leaderboard should be ranked by lower fitness if set', function tester(t) 
 
   t.deepEqual(gen.select(2), [1, 2]);
 });
+
+test('Leaderboard should be ranked by lower fitness if set', function tester(t) {
+  const fitness = (x) => x;
+  const optimize = (a,b) => a > b ? 1 : -1;
+
+  const gen = Generation.create({
+    fitness,
+    optimize,
+  }, [1, 2, 3, 4]);
+
+  t.deepEqual(gen.select(2), [1, 2]);
+});
--- tests/test-genetic.js
+++ tests/test-genetic.js
@@ -44,3 +44,23 @@ test('Optimize function should reverse result', function tester(t) {
   t.true((best.length - footprint) / best.length > 0.95);
 
 });
+
+test('Optimize function should reverse result', function tester(t) {
+  const config2 = Object.assign(config, {
+    optimize: (fit1, fit2) => {
+      return fit1 > fit2 ? 1 : -1;
+    },
+  });
+
+  config2.config.size = 2;
+
+  const tournament = new GeneticTournament(config2);
+
+  tournament.evolve();
+
+  const {best} = tournament;
+  const footprint = best.reduce((sum, occ) => sum + parseFloat(occ), 0);
+
+  t.true((best.length - footprint) / best.length > 0.5);
+
+});
